8a1880880158b554c75216ec87b8409f925332b0
Pascual Cano <pascual@allhuman.com>
2023-03-25 20:25:02 +0100

    Add conjured items

--- src/gilded-rose.ts
+++ src/gilded-rose.ts
@@ -9,6 +9,8 @@ export const updateQuality = (items: Item[]): Item[] => {
 				return getUpdatedBackstagePass(item);
 			case "Aged Brie":
 				return getUpdatedAgedBrie(item);
+			case "Conjured Mana Cake":
+				return getUpdatedConjuredItem(item);
 			default:
 				return getUpdatedItem(item);
 		}
@@ -38,5 +40,13 @@ const getUpdatedAgedBrie = (item: Item): Item => {
 	const sellIn = item.sellIn - 1;
 	const quality = sellIn < 0 ? item.quality + 2 : item.quality + 1;
 	if (quality > 50) return { name, sellIn, quality: 50 };
+	else return { name, sellIn, quality };
+};
+
+const getUpdatedConjuredItem = (item: Item): Item => {
+	const name = item.name;
+	const sellIn = item.sellIn - 1;
+	const quality = sellIn < 0 ? item.quality - 4 : item.quality - 2;
+	if (quality < 0) return { name, sellIn, quality: 0 };
 	else return { name, sellIn, quality };
 };
