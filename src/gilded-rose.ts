c528b631896a4a465d8b7000a92c23bb4d338744
Pascual Cano <pascual@allhuman.com>
2023-03-25 19:34:17 +0100

    Extract Backstage passes case to a separate function

--- src/gilded-rose.ts
+++ src/gilded-rose.ts
@@ -6,26 +6,15 @@ export const updateQuality = (items: Item[]): Item[] => {
 
 const getUpdatedItem = (prevItem: Item): Item => {
 	if (prevItem.name === "Sulfuras, Hand of Ragnaros") return prevItem;
+	if (prevItem.name === "Backstage passes to a TAFKAL80ETC concert") return getUpdatedBackstagePass(prevItem);
 	const item = { ...prevItem };
-	if (item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert") {
+	if (item.name != "Aged Brie") {
 		if (item.quality > 0) {
 			item.quality = item.quality - 1;
 		}
 	} else {
 		if (item.quality < 50) {
 			item.quality = item.quality + 1;
-			if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
-				if (item.sellIn < 11) {
-					if (item.quality < 50) {
-						item.quality = item.quality + 1;
-					}
-				}
-				if (item.sellIn < 6) {
-					if (item.quality < 50) {
-						item.quality = item.quality + 1;
-					}
-				}
-			}
 		}
 	}
 
@@ -33,12 +22,8 @@ const getUpdatedItem = (prevItem: Item): Item => {
 
 	if (item.sellIn < 0) {
 		if (item.name != "Aged Brie") {
-			if (item.name != "Backstage passes to a TAFKAL80ETC concert") {
-				if (item.quality > 0) {
-					item.quality = item.quality - 1;
-				}
-			} else {
-				item.quality = item.quality - item.quality;
+			if (item.quality > 0) {
+				item.quality = item.quality - 1;
 			}
 		} else {
 			if (item.quality < 50) {
@@ -46,5 +31,23 @@ const getUpdatedItem = (prevItem: Item): Item => {
 			}
 		}
 	}
+	return item;
+};
+
+const getUpdatedBackstagePass = (prevItem: Item): Item => {
+	const item = { ...prevItem };
+	if (item.quality < 50) item.quality = item.quality + 1;
+	if (item.sellIn < 11) {
+		if (item.quality < 50) {
+			item.quality = item.quality + 1;
+		}
+	}
+	if (item.sellIn < 6) {
+		if (item.quality < 50) {
+			item.quality = item.quality + 1;
+		}
+	}
+	item.sellIn = item.sellIn - 1;
+	if (item.sellIn < 0) item.quality = 0;
 	return item;
 };
