10afe095971c7f29ad4ae998bd8ec04715a2b677
Pascual Cano <pascual@allhuman.com>
2023-03-25 19:21:25 +0100

    Sulfuras never changes. Move condition to an early return

--- src/gilded-rose.ts
+++ src/gilded-rose.ts
@@ -5,12 +5,11 @@ export const updateQuality = (items: Item[]): Item[] => {
 };
 
 const getUpdatedItem = (prevItem: Item): Item => {
+	if (prevItem.name === "Sulfuras, Hand of Ragnaros") return prevItem;
 	const item = { ...prevItem };
 	if (item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert") {
 		if (item.quality > 0) {
-			if (item.name != "Sulfuras, Hand of Ragnaros") {
-				item.quality = item.quality - 1;
-			}
+			item.quality = item.quality - 1;
 		}
 	} else {
 		if (item.quality < 50) {
@@ -29,16 +28,14 @@ const getUpdatedItem = (prevItem: Item): Item => {
 			}
 		}
 	}
-	if (item.name != "Sulfuras, Hand of Ragnaros") {
-		item.sellIn = item.sellIn - 1;
-	}
+
+	item.sellIn = item.sellIn - 1;
+
 	if (item.sellIn < 0) {
 		if (item.name != "Aged Brie") {
 			if (item.name != "Backstage passes to a TAFKAL80ETC concert") {
 				if (item.quality > 0) {
-					if (item.name != "Sulfuras, Hand of Ragnaros") {
-						item.quality = item.quality - 1;
-					}
+					item.quality = item.quality - 1;
 				}
 			} else {
 				item.quality = item.quality - item.quality;
