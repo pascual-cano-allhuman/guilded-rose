d289b0be667ba16367034a5d6d90a28848d0100d
Pascual Cano <pascual@allhuman.com>
2023-03-25 20:20:07 +0100

    Refactor getUpdatedAgedBree

--- src/gilded-rose.ts
+++ src/gilded-rose.ts
@@ -33,10 +33,10 @@ const getUpdatedBackstagePass = (item: Item): Item => {
 	else return { name, sellIn, quality };
 };
 
-const getUpdatedAgedBrie = (prevItem: Item): Item => {
-	const item = { ...prevItem };
-	if (item.quality < 50) item.quality = item.quality + 1;
-	item.sellIn = item.sellIn - 1;
-	if (item.sellIn < 0 && item.quality < 50) item.quality = item.quality + 1;
-	return item;
+const getUpdatedAgedBrie = (item: Item): Item => {
+	const name = item.name;
+	const sellIn = item.sellIn - 1;
+	const quality = sellIn < 0 ? item.quality + 2 : item.quality + 1;
+	if (quality > 50) return { name, sellIn, quality: 50 };
+	else return { name, sellIn, quality };
 };
