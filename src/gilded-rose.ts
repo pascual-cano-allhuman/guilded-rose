b674be371f8902e980da17dd49ae50fa09b93aeb
Arek Kilimar <arek@strata3.com>
2023-03-31 12:37:47 +0100

    moving extra code from updateQuality to sperate function

--- src/gilded-rose.ts
+++ src/gilded-rose.ts
@@ -4,11 +4,25 @@ const updateQualityForRegularItems = (item: Item) => {
 	if (item.quality > 0) {
 		item.quality = item.quality - 1;
 	}
+	item.sellIn = item.sellIn - 1;
+
+	if (item.sellIn < 0) {
+		if (item.quality > 0) {
+			item.quality = item.quality - 1;
+		}
+	}
 };
 
 const updateAgedBrie = (item: Item) => {
 	if (item.quality < 50) {
 		item.quality = item.quality + 1;
+	}
+	item.sellIn = item.sellIn - 1;
+
+	if (item.sellIn < 0) {
+		if (item.quality < 50) {
+			item.quality = item.quality + 1;
+		}
 	}
 };
 
@@ -28,6 +42,11 @@ const updateQualityForBackstagePasses = (item: Item) => {
 			}
 		}
 	}
+	item.sellIn = item.sellIn - 1;
+
+	if (item.sellIn < 0) {
+		item.quality = item.quality - item.quality;
+	}
 };
 
 export const updateQuality = (prevItems: Item[]): Item[] => {
@@ -41,24 +60,6 @@ export const updateQuality = (prevItems: Item[]): Item[] => {
 			updateAgedBrie(item);
 		} else {
 			updateQualityForBackstagePasses(item);
-		}
-
-		item.sellIn = item.sellIn - 1;
-
-		if (item.sellIn < 0) {
-			if (item.name != "Aged Brie") {
-				if (item.name != "Backstage passes to a TAFKAL80ETC concert") {
-					if (item.quality > 0) {
-						item.quality = item.quality - 1;
-					}
-				} else {
-					item.quality = item.quality - item.quality;
-				}
-			} else {
-				if (item.quality < 50) {
-					item.quality = item.quality + 1;
-				}
-			}
 		}
 	});
 
