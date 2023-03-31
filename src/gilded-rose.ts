73d3bae2a024b89767ca2ccadcae6dad7434516f
STRATA3\Gustavo.Quevedo <gustavo.quevedo@strata3.com>
2023-03-31 11:44:17 +0100

    Using strict comparing

--- src/gilded-rose.ts
+++ src/gilded-rose.ts
@@ -3,16 +3,16 @@ import { Item } from "./item";
 export const updateQuality = (prevItems: Item[]): Item[] => {
 	const items = [...prevItems];
 	items.forEach(item => {
-		if (item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert") {
+		if (item.name !== "Aged Brie" && item.name !== "Backstage passes to a TAFKAL80ETC concert") {
 			if (item.quality > 0) {
-				if (item.name != "Sulfuras, Hand of Ragnaros") {
+				if (item.name !== "Sulfuras, Hand of Ragnaros") {
 					item.quality = item.quality - 1;
 				}
 			}
 		} else {
 			if (item.quality < 50) {
 				item.quality = item.quality + 1;
-				if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
+				if (item.name === "Backstage passes to a TAFKAL80ETC concert") {
 					if (item.sellIn < 11) {
 						if (item.quality < 50) {
 							item.quality = item.quality + 1;
@@ -26,14 +26,14 @@ export const updateQuality = (prevItems: Item[]): Item[] => {
 				}
 			}
 		}
-		if (item.name != "Sulfuras, Hand of Ragnaros") {
+		if (item.name !== "Sulfuras, Hand of Ragnaros") {
 			item.sellIn = item.sellIn - 1;
 		}
 		if (item.sellIn < 0) {
-			if (item.name != "Aged Brie") {
-				if (item.name != "Backstage passes to a TAFKAL80ETC concert") {
+			if (item.name !== "Aged Brie") {
+				if (item.name !== "Backstage passes to a TAFKAL80ETC concert") {
 					if (item.quality > 0) {
-						if (item.name != "Sulfuras, Hand of Ragnaros") {
+						if (item.name !== "Sulfuras, Hand of Ragnaros") {
 							item.quality = item.quality - 1;
 						}
 					}
