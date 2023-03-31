a466e80606657b6f6507b3d109bab5d61fc1c9c0
Pascual Cano <pascual@allhuman.com>
2023-03-31 13:07:38 +0200

    Exclude the case of sulfuras with an early return

--- src/gilded-rose.ts
+++ src/gilded-rose.ts
@@ -1,11 +1,8 @@
 import { Item } from "./item";
 
 const updateQualityForRegularItems = (item: Item) => {
-	
 	if (item.quality > 0) {
-		if (item.name != "Sulfuras, Hand of Ragnaros") {
-			item.quality = item.quality - 1;
-		}
+		item.quality = item.quality - 1;
 	}
 };
 
@@ -13,6 +10,7 @@ export const updateQuality = (prevItems: Item[]): Item[] => {
 	const items = [...prevItems];
 
 	items.forEach(item => {
+		if (item.name === "Sulfuras, Hand of Ragnaros") return;
 		if (item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert") {
 			updateQualityForRegularItems(item);
 		} else {
@@ -32,16 +30,14 @@ export const updateQuality = (prevItems: Item[]): Item[] => {
 				}
 			}
 		}
-		if (item.name != "Sulfuras, Hand of Ragnaros") {
-			item.sellIn = item.sellIn - 1;
-		}
+
+		item.sellIn = item.sellIn - 1;
+
 		if (item.sellIn < 0) {
 			if (item.name != "Aged Brie") {
 				if (item.name != "Backstage passes to a TAFKAL80ETC concert") {
 					if (item.quality > 0) {
-						if (item.name != "Sulfuras, Hand of Ragnaros") {
-							item.quality = item.quality - 1;
-						}
+						item.quality = item.quality - 1;
 					}
 				} else {
 					item.quality = item.quality - item.quality;
